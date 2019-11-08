3999744db8d3157ca204444bfcdff7b2b11846f1
Scaler <Scaler@itcast.cn>
2019-11-08 15:29:05 +0800

    修改工程

--- src/cn/itcast/git/FirstClass.java
+++ src/cn/itcast/git/FirstClass.java
@@ -3,7 +3,7 @@ package cn.itcast.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("这是提交的一个工程");
-    }
+    }//hello test
     public void test(){
         int i = 0;
         i += 10;
